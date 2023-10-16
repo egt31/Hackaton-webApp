00f2244264722485e57af4612900a1fe52879bb9
egt31 <147595547+egt31@users.noreply.github.com>
2023-10-16 20:58:02 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
-// EXPRESS WEB-SERVER
 const express = require('express');
 const app = express();
 const port = 3000;
-
-// Import body-parser for form data parsing
+const path = require('path');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
+
+// Set up EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
 // Use body parsing middleware to handle form data
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Serve 'test.html' as the root file
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/test.html');
-});
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-// NODE AND MYSQL CONNECTION
-const mysql = require('mysql');
 
 // Create a MySQL connection
 const connection = mysql.createConnection({
@@ -47,14 +37,41 @@ process.on('exit', () => {
     connection.end();
 });
 
+// Serve 'test.html' as the root file
+app.get('/', (req, res) => {
+    res.render('test');
+});
+
+// ... (previous code)
+
+// Define a route for /admin
+app.get('/admin', (req, res) => {
+    // Query the database to retrieve the data you need
+    const query = 'SELECT student_id, first_name, last_name, email, school_id, chosen_service_id, appointment_date, status FROM Students';
+
+    connection.query(query, (err, results) => {
+        if (err) {
+            console.error('Error querying the database: ' + err.stack);
+            // Handle the error, e.g., render an error page
+            res.render('error', { error: 'Failed to retrieve data' });
+        } else {
+            // If successful, pass the data to the EJS template
+            const data = {
+                students: results
+            };
+            console.log('Data retrieved from the database:', results); // Add this line for debugging
+            res.render('admin', data);
+        }
+    });
+});
+
 // FORM HANDLING
 app.post('/submit', (req, res) => {
-    const { first_name, last_name, email, chosen_service_id, appointment_date, studentID } = req.body;
+    const { first_name, last_name, email, chosen_service_id, appointment_date, school_id } = req.body;
 
-    const insertQuery = 'INSERT INTO students (first_name, last_name, email, chosen_service_id, appointment_date, studentID) VALUES (?, ?, ?, ?, ?, ?)';
+    const insertQuery = 'INSERT INTO students (first_name, last_name, email, chosen_service_id, appointment_date, school_id, status) VALUES (?, ?, ?, ?, ?, ?, "Pending")';
 
-
-    connection.query(insertQuery, [first_name, last_name, email, chosen_service_id, appointment_date, studentID], (err, result) => {
+    connection.query(insertQuery, [first_name, last_name, email, chosen_service_id, appointment_date, school_id], (err, result) => {
         if (err) {
             console.error('Error inserting data into the database: ' + err.stack);
             res.status(500).send('Error submitting the form');
@@ -65,4 +82,23 @@ app.post('/submit', (req, res) => {
     });
 });
 
+// Update status (admin functionality)
+app.post('/update-status', (req, res) => {
+    const { student_id, status } = req.body;
 
+    // Call the stored procedure
+    connection.query('CALL UpdateStudentStatus(?, ?)', [student_id, status], (err, result) => {
+        if (err) {
+            console.error('Error calling the stored procedure: ' + err.stack);
+            res.status(500).send('Error updating status');
+        } else {
+            console.log('Status updated in the database');
+            res.status(200).send('Status updated successfully');
+        }
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
